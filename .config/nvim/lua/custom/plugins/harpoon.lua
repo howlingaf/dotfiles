14042b59e54107a9570c4dc9c15c1f04bf02301c
howlingaf <github@howlingaf.gg>
2026-07-09 19:32:36 -0500

    add harpoon config

---
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -0,0 +1,21 @@
+require('harpoon'):setup {}
+
+vim.keymap.set('n', '<leader>a', function()
+  require('harpoon'):list():add()
+end, { desc = 'Harpoon add file' })
+vim.keymap.set('n', '<C-e>', function()
+  local harpoon = require 'harpoon'
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = 'Harpoon menu' })
+vim.keymap.set('n', '<C-q>', function()
+  require('harpoon'):list():select(1)
+end, { desc = 'Harpoon file 1' })
+vim.keymap.set('n', '<C-w>', function()
+  require('harpoon'):list():select(2)
+end, { nowait = true, desc = 'Harpoon file 2' })
+vim.keymap.set('n', '<C-a>', function()
+  require('harpoon'):list():select(3)
+end, { desc = 'Harpoon file 3' })
+vim.keymap.set('n', '<C-s>', function()
+  require('harpoon'):list():select(4)
+end, { desc = 'Harpoon file 4' })
